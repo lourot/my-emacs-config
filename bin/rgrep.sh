bbecd7585e06d193f3f551a9197dcb8d767a0b8e
Aurelien Lourot <aurelien.lourot@gmail.com>
2018-08-18 22:55:17 +0300

    Removed pager from rgrep script.

--- bin/rgrep.sh
+++ bin/rgrep.sh
@@ -3,14 +3,14 @@
 # * https://askubuntu.com/questions/920908/what-does-export-term-linux-actually-do-when-inside-a-script
 export TERM=linux
 
-(set -x; git grep --untracked -n "$@")
+(set -x; git --no-pager grep --untracked -n "$@")
 code=$?
 printf "\n\n\n\n\n"
 
 if [ "$code" -eq "0" ]; then
-  (set -x; git grep --recurse-submodules -n "$@")
+  (set -x; git --no-pager grep --recurse-submodules -n "$@")
   printf "\n\n\n\n\n"
 else
-  (set -x; git grep --no-index --exclude-standard -n "$@")
+  (set -x; git --no-pager grep --no-index --exclude-standard -n "$@")
   printf "\n\n\n\n\n"
 fi

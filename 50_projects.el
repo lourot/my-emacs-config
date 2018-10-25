d3aa38405153e712970ae22c98e16a708f03628c
Aurelien Lourot <aurelien.lourot@gmail.com>
2018-10-25 10:19:28 +0200

    Removed project-specific python mode.

--- 50_projects.el
+++ 50_projects.el
@@ -2,15 +2,6 @@
 
 (setq esys-tabwidth 8)
 
-(defun esys-python-mode ()
-  "ESYS Python mode"
-  (python-mode)
-  (setq tab-width esys-tabwidth) ;; display width of character TAB
-  (setq indent-tabs-mode t) ;; indent with TAB instead of blanks
-  (setq mode-name "ESYS Python") ;; must be the last one
-)
-(add-to-list 'auto-mode-alist '("/esys/.*\\.py\\'" . esys-python-mode))
-
 (defun esys-conf-mode ()
   "ESYS Conf mode"
   (conf-colon-mode)

f4b0a3f4265e7cfc3d52b22ba06519aee155e2a1
Aurelien Lourot <aurelien.lourot@gmail.com>
2018-10-31 19:24:39 +0100

    Removed project-specific sql mode.

--- 50_projects.el
+++ 50_projects.el
@@ -2,15 +2,6 @@
 
 (setq esys-tabwidth 8)
 
-(defun esys-sql-mode ()
-  "ESYS SQL mode"
-  (sql-mode)
-  (setq tab-width esys-tabwidth) ;; display width of character TAB
-  (setq indent-tabs-mode t) ;; indent with TAB instead of blanks
-  (setq mode-name "ESYS SQL") ;; must be the last one
-)
-(add-to-list 'auto-mode-alist '("/esys/.*\\.sql\\'" . esys-sql-mode))
-
 (defun esys-sh-mode ()
   "ESYS SH mode"
   (interactive)

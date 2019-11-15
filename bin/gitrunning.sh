efac62a8323afe2d3701f97d02e5c83c3b022dbd
Aurelien Lourot <aurelien.lourot@gmail.com>
2019-11-15 14:20:03 +0100

    Git detection improved.

--- bin/gitrunning.sh
+++ bin/gitrunning.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Returns a positive number if a git process is running, 0 otherwise.  
-num=`pgrep -lfa git | grep -v gitrunning | grep -v daemon | grep -v emacsclient | grep -v reframe.start | wc -l`
+num=`pgrep -lfa git | grep -v gitrunning | grep -v daemon | grep -v emacsclient | grep -v reframe.start | grep -v openvpn | wc -l`
 exit $num

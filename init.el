fd57204fcdfb9d416b79312a6feadfd91f36a0cd
Aurelien Lourot <aurelien.lourot@gmail.com>
2022-11-12 23:30:41 +0100

    Enable typescript-mode

--- init.el
+++ init.el
@@ -57,7 +57,7 @@
  ;; If there is more than one, they won't work right.
  '(Buffer-menu-name-width 30)
  '(auth-source-save-behavior nil)
- '(package-selected-packages '(ggtags elpy))
+ '(package-selected-packages '(typescript-mode ggtags elpy))
  '(tab-stop-list
    '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120)))
 (custom-set-faces
